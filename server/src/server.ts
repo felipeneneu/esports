be696af746de6bcb7b50aac8744bf46b428a1dba
felipeneneu <feliperpg3@gmail.com>
2024-04-01 11:50:00 -0300

    Add Cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import cors from 'cors'
+
 import { PrismaClient } from '@prisma/client'
 import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes'
 import { convertMinutesToHourString } from './utils/convert-minutes-string-to-hours'
@@ -10,6 +12,7 @@ const prisma = new PrismaClient({
 })
 
 app.use(express.json())
+app.use(cors())
 
 // www.minhaapi.com/ads
 
